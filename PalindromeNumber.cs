c9d954cd039557d5aaf486e3106680e3a7ca42a6
sachin-em <sachinmeledath99@gmail.com>
2025-01-20 18:40:19 +0530

    Palindrome number solution

---
+++ PalindromeNumber.cs
@@ -0,0 +1,20 @@
+public class Solution {
+    public bool IsPalindrome(int x) {
+        int og = x;
+        if (x < 0)
+            return false;
+
+        int reversedDigit = 0;
+        
+        while(x > 0) {
+            int lastDigit = x % 10;
+            reversedDigit = reversedDigit * 10 + lastDigit;
+            x /= 10;
+        }
+
+        if (reversedDigit == og)
+            return true;
+        
+        return false;
+    }
+}
