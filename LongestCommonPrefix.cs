6d44859d52ad9c017bfb557115520ff483bcceb2
sachin-em <sachinmeledath99@gmail.com>
2025-01-21 20:53:14 +0530

    Longest common prefix leetcode problem solution

---
+++ LongestCommonPrefix.cs
@@ -0,0 +1,35 @@
+public class Solution {
+    public string LongestCommonPrefix(string[] strs) {
+        string smallestStr = strs[0], prefix = string.Empty;
+        int charIdx = -1, minLength = strs[0].Length;
+
+        for (int i = 0; i < strs.Length; i++) {
+            string currentStr = strs[i];
+            if (currentStr.Length < minLength){
+                minLength = currentStr.Length;
+                smallestStr = currentStr;
+            }
+        }
+
+        foreach (char letter in smallestStr) {
+            charIdx++;
+            bool isLetterMatched = true;
+            for (int i = 0; i < strs.Length; i++) {
+                string currentStr = strs[i];
+
+                if (currentStr[charIdx] != letter) {
+                    isLetterMatched = false;
+                    break;
+                }
+            }
+
+            if (isLetterMatched){
+                prefix += letter.ToString();
+            } else {
+                break;
+            }
+        }
+
+        return prefix;
+    }
+}
